5a2f052a4d6ac788fcedad8c5bde224a0018870c
Mahrad <mahradti@gmail.com>
2024-09-03 19:44:46 +0330

    CartModal was created

--- components/CartModal.tsx
+++ components/CartModal.tsx
@@ -1,9 +1,56 @@
 "use client"
 
-const CartModal = ()=>{
+import Image from "next/image";
+
+const CartModal = () => {
+
+    const cartItem = true
+
     return (
-        <div>
-            CartModal
+        <div className="w-max absolute p-4 rounded-md shadow-lg bg-white top-12 right-0 flex flex-col gap-6 z-20">
+            {!cartItem ? (
+                <div>Cart is Empty</div>
+            ) : (
+               <>
+               <h2 className="text-xl">Shopping Cart</h2>
+                <div className="flex flex-col gap-8">
+                    {/* ITEM */}
+                    <div className="flex gap-4">
+                        <Image src={"/woman.png"} alt="" width={72} height={96} className="object-cover rounded-md" />
+                        <div className="flex flex-col justify-between w-full">
+                            {/* TOP */}
+                            <div>
+                                <div className="flex items-center justify-between gap-8">
+                                    <h3 className="font-semibold">Product Name</h3>
+                                    <div className="p-1 bg-gray-50 rounded-sm">$49</div>
+                                </div>
+                                <div className="text-sm text-gray-500">
+                                    Available
+                                </div>
+                            </div>
+                            {/* Bottom */}
+                            <div className="flex justify-between text-sm">
+                                <span className="text-gray-500">Qty. 2</span>
+                                <span className="text-blue-500">Remove</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className="">
+                   <div className="flex justify-between items-center font-semibold">
+                       <span className="">Subtotal</span>
+                       <span className="">$49</span>
+                   </div>
+                   <p className="text-gray-500 text-sm mt-2 mb-4">Lorem ipsum dolor sit amet, consectetur</p>
+                   <div className="flex justify-between text-sm">
+                       <button className="rounded-sm py-3 px-4 ring-1 ring-gray-300">View Cart</button>
+                       <button className="rounded-sm py-3 px-4 bg-black text-white">Checkout</button>
+                   </div>
+                </div>
+              </> 
+            )}
         </div>
     )
 }
