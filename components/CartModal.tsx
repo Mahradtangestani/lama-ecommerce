1536cff02989da4186614cda344dce65b19993f2
Mahrad <mahradti@gmail.com>
2024-09-04 13:53:52 +0330

    Slider was created

--- components/CartModal.tsx
+++ components/CartModal.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 
 const CartModal = () => {
 
+
     const cartItem = true
 
     return (
--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        remotePatterns:[
-            {
-                protocol:"https",
-                hostname:"images.pexcels.com"
-            }
-
-        ]
-    }
-};
-
-export default nextConfig;
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "images.pexels.com",
+        },
+        {
+          protocol: "https",
+          hostname: "static.wixstatic.com",
+        },
+        {
+          protocol: "https",
+          hostname: "people.pic1.co",
+        },
+        {
+          protocol: "https",
+          hostname: "app-uploads-cdn.fera.ai",
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
