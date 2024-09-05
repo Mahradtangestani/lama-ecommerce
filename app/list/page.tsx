ab3ecc67a533d306eb742c71a87ad33955e7209a
Mahrad <mahradti@gmail.com>
2024-09-05 14:34:16 +0330

    ListPage's Banner created

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -1,7 +1,17 @@
+import Image from "next/image";
+
 const ListPage = ()=>{
     return (
-        <div>
-            List Page
+        <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
+            <div className="bg-pink-50 px-4 flex justify-between h-64">
+                <div className="w-2/3 flex flex-col justify-center items-center gap-8">
+                  <h1 className="text-4xl font-semibold leading-[48px] text-gray-700">Grap up to 50% of on <br /> Selected Products</h1>
+                  <button className="rounded-3xl bg-red-500 text-white text-sm w-max py-3 px-5 hover:bg-white hover:text-red-500 hover:ring-1 hover:ring-red-500 hover:rounded-3xl">Buy Now</button>
+                </div>
+                <div className="relative w-1/3">
+                    <Image src={'/woman.png'} alt="" fill className="object-contain"/>
+                </div>
+            </div>
         </div>
     )
 }
