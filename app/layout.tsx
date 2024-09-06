8efef54df60494fb806e96a313e36435dbfd2b44
Mahrad <mahradti@gmail.com>
2024-09-06 17:07:12 +0330

    WixContext created

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { WixClientContextProvider } from "@/context/wixContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,9 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <WixClientContextProvider>
         <Navbar/>
         {children}
         <Footer/>
+        </WixClientContextProvider>
       </body>
     </html>
   );
